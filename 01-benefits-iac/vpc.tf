a3e0a7647f12dd6f94dabad38ac3905f5fb519c3
Titus Lim <tituslhy@gmail.com>
2026-02-06 22:18:27 +0800

    create first terraform file

---
+++ 01-benefits-iac/vpc.tf
@@ -0,0 +1,54 @@
+// Declaring providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+// configure region
+provider "aws" {
+  region = "us-east-1"
+}
+
+// Creating a VPC with a specified cinder block
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+// Creating a public subnet in the VPC
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.0.0/24"
+}
+
+// Creating a private subnet in the VPC
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+// Creating an internet gateway for the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.demo_vpc.id
+}
+
+// Create a route table for the VPC
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  // Adding a route to allow internet access
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associating the route table with the public subnet so the public subnet
+# has internet access. 
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rtb.id
+}
