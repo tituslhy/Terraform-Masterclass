f30d3ec98ba618ce03842bce6f49fd5b41888dc9
Titus Lim <tituslhy@gmail.com>
2026-02-06 22:42:23 +0800

    run first terraform command

--- 01-benefits-iac/vpc.tf
+++ 01-benefits-iac/vpc.tf
@@ -16,6 +16,10 @@ provider "aws" {
 // Creating a VPC with a specified cinder block
 resource "aws_vpc" "demo_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Terraform VPC"
+  }
 }
 
 // Creating a public subnet in the VPC
