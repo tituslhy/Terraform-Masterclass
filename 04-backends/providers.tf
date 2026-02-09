2428e8ed06a5d44293f882209ec444c5d6b1886e
Titus Lim <tituslhy@gmail.com>
2026-02-09 14:24:02 +0800

    add backend

---
+++ 04-backends/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    # Generate random sequences of strings/id
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  # Creating an s3 backend. Notice that terraform does not create
+  # this backend s3 for us! So it must be present to begin with.
+  backend "s3" {
+    bucket = "terraform-course-titus-remote-backend"
+    key    = "04-backends/state.tfstate"
+    region = "us-east-1"
+    # dynamo db tables can be used for locking purposes
+    # Note that not all backends allow for locking
+    # dynamodb_table = "my-dynamodb-table"
+  }
+}
+
+# The region here is where the resources will be deployed.
+provider "aws" {
+  region = "us-east-1"
+}
